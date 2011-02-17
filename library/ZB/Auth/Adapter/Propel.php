56e1967a30d4b7a2e4db69111384b2c1ee0c7bb7
Dan Dart <dandart@googlemail.com>
2011-02-17 23:28:13 +0000

    auth adapter

---
+++ library/ZB/Auth/Adapter/Propel.php
@@ -0,0 +1,39 @@
+<?php
+class ZB_Auth_Adapter_Propel implements Zend_Auth_Adapter_Interface
+{
+    private $_username;
+    private $_password;
+
+    public function __construct($username, $password)
+    {
+        $this->_username = strtolower($username);
+        $this->_password = $password;
+    }
+
+    public function authenticate()
+    {
+        $identity = UserQuery::create()
+            ->filterByEmail($this->_email)
+            ->findOne();
+
+        if(is_null($identity))
+        {
+            $identity = UserQuery::create()
+                ->filterByUsername($this->_username)
+                ->findOne();
+        }
+
+        if(is_null($identity))
+        {
+            return new Zend_Auth_Result(Zend_Auth_Result::FAILURE_IDENTITY_NOT_FOUND, null, array('Login Failed.'));
+        }
+
+        if(User::encryptPassword($this->_password) == $identity->getPassword())
+        {
+            return new Zend_Auth_Result(Zend_Auth_Result::SUCCESS, $identity);
+        }
+        
+        return new Zend_Auth_Result(Zend_Auth_Result::FAILURE_IDENTITY_NOT_FOUND, null, array('Login Failed.')); 
+    }
+}
+
